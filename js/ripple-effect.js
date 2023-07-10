5d83acc47adf3dad72d6038b1a51810a46a05e88
RubenVeraJinich <35475345+RubenVeraJinich@users.noreply.github.com>
2023-07-10 01:50:21 -0600

    Add files via upload
    Hace falta subir la carpeta assets y organizar todo

---
+++ js/ripple-effect.js
@@ -0,0 +1,22 @@
+// JavaScript Document  RIPPLE EFFECT
+
+
+$('.btn').ripple({
+  duration: 1000,
+});
+
+$('.menu-custom .nav-link').ripple({
+  duration: 1000,
+});
+
+$('.menu-pills .nav-link').ripple({
+  duration: 1000,
+});
+
+$('.nav-tabs .nav-link').ripple({
+  duration: 1000,
+});
+
+$('.page-item.active .page-link').ripple({
+  duration: 1000,
+});
